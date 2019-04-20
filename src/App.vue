b5170251b39c50fa274fec7970f9195c541ddb31
DESKTOP-3QJGL4M\titan <titanmar15@gmail.com>
2019-04-20 14:10:07 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Menu />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Menu from "@/components/Menu.vue";
+export default {
+  components: {
+    Menu
+  },
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="menu">
+    <h1>hola mundo</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
